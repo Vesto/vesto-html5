c3b1abbba71ea921f65b88449dab1bb7bb540c27
Nathan Flurry <developer@nathanflurry.com>
2016-12-18 18:36:05 -0700

    Testing some more properties of `View`

--- client/app.ts
+++ client/app.ts
@@ -1,5 +1,5 @@
 import "quark-html5"; // Modifies the prototypes automatically
-import { View, ViewBacking, Color, Rect } from "quark";
+import { View, ViewBacking, Color, Rect, Shadow, Point } from "quark";
 
 // Get the current version
 document.write(`The current version is ${process.version}`);
@@ -15,6 +15,15 @@ documentView.addSubview(rootView);
 let view = new View();
 view.rect = new Rect(10, 10, 100, 100);
 view.backgroundColor = new Color(0, 1, 0, 1);
+view.shadow = new Shadow(new Point(0, 10), 10, new Color(0, 0, 1, 0.5));
+view.cornerRadius = 8;
 rootView.addSubview(view);
+
+setInterval(() => {
+    view.alpha = Math.random();
+}, 500);
+setInterval(() => {
+    view.isHidden = !view.isHidden;
+}, 100);
 
 console.log("Done");
