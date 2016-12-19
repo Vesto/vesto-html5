d2e510e23330c01ff5e092473e749765e9205609
Nathan Flurry <developer@nathanflurry.com>
2016-12-19 13:21:47 -0700

    Added `DraggableView` test Now enables `PointerEvent` blink features

--- client/app.ts
+++ client/app.ts
@@ -1,8 +1,43 @@
 import "quark-html5"; // Modifies the prototypes automatically
-import { View, ViewBacking, Color, Rect, Shadow, Point } from "quark";
+import { View, ViewBacking, Color, Rect, Shadow, Point, InteractionEvent, EventPhase, InteractionType } from "quark";
 
 // Get the current version
 document.write(`The current version is ${process.version}`);
+
+class DraggableView extends View {
+    private previousLocation?: Point;
+
+    interactionEvent(event: InteractionEvent): boolean {
+        console.log("Interaction");
+        if (event.type == InteractionType.LeftMouse) {
+            if (event.phase == EventPhase.Began)
+                this.previousLocation = event.location;
+            else if (event.phase == EventPhase.Changed)
+                this.updatePosition(event.location);
+            else if (event.phase == EventPhase.Ended || event.phase == EventPhase.Cancelled)
+                this.previousLocation = undefined;
+            return true;
+        }
+
+        return super.interactionEvent(event);
+    }
+
+    private updatePosition(location: Point) {
+        // Make sure there's a previous position
+        if (typeof this.previousLocation == "undefined")
+            return;
+
+        // Move the view
+        let delta = location.add(this.previousLocation.inverse());
+        this.rect = new Rect(
+            this.rect.x + delta.x, this.rect.y + delta.y,
+            this.rect.width, this.rect.height
+        );
+
+        // Save the previous position
+        this.previousLocation = location;
+    }
+}
 
 // let rootView = new QKView(document.body);
 let rootView = new View();
@@ -17,7 +52,7 @@ view.rect = new Rect(10, 10, 100, 100);
 view.backgroundColor = new Color(0, 1, 0, 1);
 view.shadow = new Shadow(new Point(0, 10), 10, new Color(0, 0, 1, 0.5));
 view.cornerRadius = 8;
-rootView.addSubview(view);
+// rootView.addSubview(view);
 
 setInterval(() => {
     view.alpha = Math.random();
@@ -25,5 +60,11 @@ setInterval(() => {
 setInterval(() => {
     view.isHidden = !view.isHidden;
 }, 100);
+
+let draggable = new DraggableView();
+draggable.rect = new Rect(50, 50, 100, 100);
+draggable.backgroundColor = new Color(1, 0, 1, 1);
+rootView.addSubview(draggable);
+
 
 console.log("Done");
--- server/index.ts
+++ server/index.ts
@@ -3,13 +3,16 @@ import { app, BrowserWindow } from 'electron';
 // Get a path to where the client folder is located with the resources
 let clientPath = `file://${__dirname}/../client`;
 
-// Create the window whenever ready
+// Create the window whenever ready // TODO: Use window.setThumbarButtons ?
 app.on('ready', () => {
     let mainWindow = new BrowserWindow({
         width: 800,
         height: 800,
         frame: false, // Don't add a frame
-        titleBarStyle: 'hidden-inset' // Inset the buttons so can show custom title bar
+        titleBarStyle: 'hidden-inset', // Inset the buttons so can show custom title bar
+        webPreferences: {
+            blinkFeatures: "PointerEvent" // Enables pointer events
+        }
     });
     mainWindow.loadURL(`${clientPath}/index.html`);
 });
