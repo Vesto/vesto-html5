3cdf93bdd53243d83effa2b8d9805dcdd81e9ab3
Nathan Flurry <developer@nathanflurry.com>
2016-12-22 00:42:25 -0700

    Now loads `test-module` instead of using internal classes

--- client/app.ts
+++ client/app.ts
@@ -1,70 +1,20 @@
-import "quark-html5"; // Modifies the prototypes automatically
-import { View, ViewBacking, Color, Rect, Shadow, Point, InteractionEvent, EventPhase, InteractionType } from "quark";
+import { QKInstance, QKModule, QKWindow } from "quark-html5"; // Modifies the prototypes automatically
+import * as URI from "urijs";
 
 // Get the current version
 document.write(`The current version is ${process.version}`);
 
-class DraggableView extends View {
-    private previousLocation?: Point;
+// Create an element
+let element = document.createElement("div");
+element.style.left = "10px";
+element.style.right = "10px";
+element.style.top = "10px";
+element.style.bottom = "10px";
+document.body.appendChild(element);
 
-    interactionEvent(event: InteractionEvent): boolean {
-        console.log("Interaction");
-        if (event.type == InteractionType.LeftMouse) {
-            if (event.phase == EventPhase.Began)
-                this.previousLocation = event.location;
-            else if (event.phase == EventPhase.Changed)
-                this.updatePosition(event.location);
-            else if (event.phase == EventPhase.Ended || event.phase == EventPhase.Cancelled)
-                this.previousLocation = undefined;
-            return true;
-        }
+// Create a window
+let window = new QKWindow(element);
 
-        return super.interactionEvent(event);
-    }
-
-    private updatePosition(location: Point) {
-        // Make sure there's a previous position
-        if (typeof this.previousLocation == "undefined")
-            return;
-
-        // Move the view
-        let delta = location.add(this.previousLocation.inverse());
-        this.rect = new Rect(
-            this.rect.x + delta.x, this.rect.y + delta.y,
-            this.rect.width, this.rect.height
-        );
-
-        // Save the previous position
-        this.previousLocation = location;
-    }
-}
-
-// let rootView = new QKView(document.body);
-let rootView = new View();
-rootView.rect = new Rect(10, 10, 300, 300);
-rootView.backgroundColor = new Color(1, 0, 0, 1);
-
-let documentView = new View(document.body as ViewBacking);
-documentView.addSubview(rootView);
-
-let view = new View();
-view.rect = new Rect(10, 10, 100, 100);
-view.backgroundColor = new Color(0, 1, 0, 1);
-view.shadow = new Shadow(new Point(0, 10), 10, new Color(0, 0, 1, 0.5));
-view.cornerRadius = 8;
-// rootView.addSubview(view);
-
-setInterval(() => {
-    view.alpha = Math.random();
-}, 500);
-setInterval(() => {
-    view.isHidden = !view.isHidden;
-}, 100);
-
-let draggable = new DraggableView();
-draggable.rect = new Rect(50, 50, 100, 100);
-draggable.backgroundColor = new Color(1, 0, 1, 1);
-rootView.addSubview(draggable);
-
-
-console.log("Done");
+// Create and start the instance
+let instance = new QKInstance(new QKModule(new URI("/Users/NathanFlurry/Documents/Dev/Vesto/Code/test-module")));
+instance.start(window);
