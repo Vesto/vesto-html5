3b056b4d8feb5f4e3471bdc34a1bc87e7a231c0e
Nathan Flurry <developer@nathanflurry.com>
2016-12-28 21:04:31 -0700

    Changed the `rootElement` so it's styled in the proper place to override Quarks default styling

--- client/app.ts
+++ client/app.ts
@@ -2,16 +2,21 @@ import { QKInstance, QKModule, QKWindow } from "quark-html5"; // Modifies the pr
 import * as URI from "urijs";
 
 // Get the current version
-document.write(`The current version is ${process.version}`);
+// document.write(`The current version is ${process.version}`);
 
 // Create an rootElement
 let rootElement = document.createElement("div");
-rootElement.style.left = "30px";
-rootElement.style.right = "0";
-rootElement.style.top = "30px";
-rootElement.style.bottom = "0";
 document.body.appendChild(rootElement);
 
 // Create and start the instance
 let instance = new QKInstance(new QKModule(new URI("/Users/NathanFlurry/Documents/Dev/Vesto/Code/test-module")));
 instance.start(rootElement);
+
+// Style element after start because RootView modifies the CSS
+rootElement.style.position = "absolute";
+rootElement.style.width = "";
+rootElement.style.height = "";
+rootElement.style.left = "0";
+rootElement.style.right = "0";
+rootElement.style.top = "0";
+rootElement.style.bottom = "0";
