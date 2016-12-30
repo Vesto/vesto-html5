e19954a7864fbff5388f0f3cf51f63bb059a8c19
Nathan Flurry <developer@nathanflurry.com>
2016-12-29 19:01:09 -0700

    Now styles `QKView` in appropriate position Limits zooming with `webFrame.setLayoutZoomLevelLimits` Added `electron-rebuilt` for fixing compiling of native packages to match the property Node version

--- client/app.ts
+++ client/app.ts
@@ -1,22 +1,22 @@
-import { QKInstance, QKModule, QKWindow } from "quark-html5"; // Modifies the prototypes automatically
+import { QKInstance, QKModule, QKView } from "quark-html5"; // Modifies the prototypes automatically
+import { webFrame } from "electron";
 import * as URI from "urijs";
 
-// Get the current version
-// document.write(`The current version is ${process.version}`);
+// Prevent zooming in the viewport
+webFrame.setLayoutZoomLevelLimits(0, 0);
+// webFrame.setVisualZoomLevelLimits(0, 0); // TODO: This prevents trackpad two finger double tap) scrolling, but crashes Electron
 
-// Create an rootElement
-let rootElement = document.createElement("div");
-document.body.appendChild(rootElement);
+// Style root view to fit the viewport
+let rootView = new QKView();
+rootView.style.position = "absolute";
+rootView.style.width = "";
+rootView.style.height = "";
+rootView.style.left = "0";
+rootView.style.right = "0";
+rootView.style.top = "0";
+rootView.style.bottom = "0";
+document.body.appendChild(rootView);
 
 // Create and start the instance
 let instance = new QKInstance(new QKModule(new URI("/Users/NathanFlurry/Documents/Dev/Vesto/Code/test-module")));
-instance.start(rootElement);
-
-// Style element after start because RootView modifies the CSS
-rootElement.style.position = "absolute";
-rootElement.style.width = "";
-rootElement.style.height = "";
-rootElement.style.left = "0";
-rootElement.style.right = "0";
-rootElement.style.top = "0";
-rootElement.style.bottom = "0";
+instance.start(rootView);
